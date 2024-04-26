f04f1c61a257803653d5ebd3345bf3d17144b623
Denis Barsukov <d.barsukov@m-developer.ru>
2024-04-26 17:36:16 +0300

    hw01 disable connection pool, fix test, remove unnecessary dependencies

--- src/test/java/ru/otus/dbarsukov/ha/profiles/ProfileApplicationTests.java
+++ src/test/java/ru/otus/dbarsukov/ha/profiles/ProfileApplicationTests.java
@@ -3,7 +3,7 @@ package ru.otus.dbarsukov.ha.profiles;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(properties = { "spring.liquibase.enabled=false" })
 class ProfileApplicationTests {
 
     @Test
