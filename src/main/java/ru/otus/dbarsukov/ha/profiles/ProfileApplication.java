98c568bae5632fa95788935412b5edab4c282f00
Denis Barsukov <d.barsukov@m-developer.ru>
2024-04-26 16:12:45 +0300

    hw01 initial commit

---
+++ src/main/java/ru/otus/dbarsukov/ha/profiles/ProfileApplication.java
@@ -0,0 +1,13 @@
+package ru.otus.dbarsukov.ha.profiles;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProfileApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProfileApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ru/otus/dbarsukov/ha/profiles/ProfileApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.otus.dbarsukov.ha.profiles;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProfileApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
